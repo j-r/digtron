c74569ef5d7b0176bccd36a48a8c42247bc338bb
Jürgen Rühle <j-r@online.de>
2024-12-29 19:41:38 +0100

    Start auto controller on enter in cycles field

--- nodes/node_controllers.lua
+++ nodes/node_controllers.lua
@@ -374,7 +374,7 @@ minetest.register_node("digtron:auto_controller", {
 			meta:set_int("cycles", math.floor(cycles))
 			if sender:is_player() and cycles > 0 then
 				meta:set_string("triggering_player", sender:get_player_name())
-				if fields.execute then
+				if fields.execute or fields.key_enter_field == "cycles" then
 					auto_cycle(pos)
 				end
 			end
